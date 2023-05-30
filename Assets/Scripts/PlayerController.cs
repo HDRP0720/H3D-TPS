57ad4e019fbedf825d81075721452a6c114fa8f8
HDRP <hdrp0720@gmail.com>
2023-05-30 14:48:49 +0900

    add acceleration & change player move logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,30 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 2f;
+  [SerializeField] private float maxForwardSpeed = 8f;
+
+  private float desiredForwardSpeed;
+  private float forwardSpeed;
+
+  private const float groundAccel = 5f;
+  private const float groundDecel = 25f;
 
   private Vector2 moveDirection;
+  private Animator animator;
+
+  bool IsMoveInput
+  {
+    get { return !Mathf.Approximately(moveDirection.sqrMagnitude, 0f); }
+  }
+
+  private void Start() 
+  {
+    animator = GetComponent<Animator>();
+  }
+  private void Update()
+  {
+    Move(moveDirection);
+  }
 
   // For unity event of input system
   public void OnMove(InputAction.CallbackContext context)
@@ -17,11 +39,14 @@ public class PlayerController : MonoBehaviour
 
   private void Move(Vector2 direction)
   {
-    transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
-  }
+    if(direction.sqrMagnitude > 1f) direction.Normalize();
 
-  private void Update()
-  {
-    Move(moveDirection);
+    desiredForwardSpeed = direction.magnitude * maxForwardSpeed;
+    float acceleration = IsMoveInput ? groundAccel : groundDecel;
+
+    forwardSpeed = Mathf.MoveTowards(forwardSpeed, desiredForwardSpeed, acceleration * Time.deltaTime);
+    animator.SetFloat("ForwardSpeed", forwardSpeed);
+
+    // transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
   }
 }
